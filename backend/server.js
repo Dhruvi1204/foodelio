8d0c54101db5736933df9f35ecdec063fb4da897
Mehta Dhruvi <dhruvimehta1212@gmail.com>
2026-04-28 11:43:14 +0530

    Allow both local and production frontend

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:3000",
     "https://frabjous-banoffee-8f85d1.netlify.app"
   ],
   credentials: true
